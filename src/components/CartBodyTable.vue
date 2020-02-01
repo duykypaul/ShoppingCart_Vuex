b4ee9f370688d3b267fc4c531d9474ee9bc97662
duykypaul <duykypaul@gmail.com>
2020-02-01 13:35:11 +0700

    State & Empty [Add product to Cart]

--- src/components/CartBodyTable.vue
+++ src/components/CartBodyTable.vue
@@ -1,22 +1,35 @@
 <template>
-  <tbody id="my-cart-body">
-  <!-- CART BODY -->
-    <cart-body-item/>
-    <cart-body-item/>
-  </tbody>
+    <tbody id="my-cart-body">
+    <!-- CART BODY -->
+        <template v-if="carts.length">
+            <cart-body-item v-for="(item, index) in carts" :key="index"/>
+        </template>
+        <tr v-else>
+            <th colspan="6">{{NOTI_EMPTY}}</th>
+        </tr>
+    
+    </tbody>
 </template>
 
 <script>
-import CartBodyItem from "./CartBodyItem";
-export default {
-    name: 'cart-body-table',
-    components: {CartBodyItem},
-    data (){
-        return {
+    import {NOTI_EMPTY} from '../constants/config';
+    import {mapState} from 'vuex';
+    import CartBodyItem from "./CartBodyItem";
 
+    export default {
+        name: 'cart-body-table',
+        components: {CartBodyItem},
+        data() {
+            return {
+                NOTI_EMPTY
+            }
+        },
+        computed: {
+            ...mapState({
+                carts: state => state.cart.cartList
+            })
         }
     }
-}
 </script>
 
 <style>
---
+++ src/constants/config.js
@@ -0,0 +1,5 @@
+const NOTI_EMPTY = 'Empty product in your cart';
+
+export {
+    NOTI_EMPTY
+}
